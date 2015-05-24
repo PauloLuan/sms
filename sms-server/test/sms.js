f2d760516b011c2e7b3e68427d423f0e4c6de90b
Paulo Luan <pauloluan.inova@gmail.com>
2015-05-24 01:58:11 -0300

    added params for post requests

--- sms-server/test/sms.js
+++ sms-server/test/sms.js
@@ -8,6 +8,7 @@ describe('Assertions for /sms endpoint', function () {
     it('/sms/text should respond with status 200', function (done) {
         request(app)
             .post('/sms/text')
+            .send({text:'test'})
             .expect('Content-Type', /json/)
             .expect(200)
             .end(function (err, res) {
@@ -19,6 +20,7 @@ describe('Assertions for /sms endpoint', function () {
     it('/sms/number/ should respond with status 200', function (done) {
         request(app)
             .post('/sms/number')
+            .send({text:'test'})
             .expect('Content-Type', /json/)
             .expect(200)
             .end(function (err, res) {
