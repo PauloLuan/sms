e3a9f1e05e833560154d948d874f8fb8487673b7
Paulo Luan <pauloluan.inova@gmail.com>
2015-05-24 03:49:37 -0300

    first implementation for text to number.

--- sms-server/routes/sms.js
+++ sms-server/routes/sms.js
@@ -1,21 +1,78 @@
+'use strict';
+
+var exports = module.exports = {};
+
 var express = require('express');
 var router = express.Router();
+var _ = require('lodash');
 
-
-var textToNumber = function() {
-	
+var keyboard = {
+    '2': ['a', 'b', 'c'],
+    '3': ['d', 'e', 'f'],
+    '4': ['g', 'h', 'i'],
+    '5': ['j', 'k', 'l'],
+    '6': ['m', 'n', 'o'],
+    '7': ['p', 'q', 'r', 's'],
+    '8': ['t', 'u', 'v'],
+    '9': ['w', 'x', 'y', 'z'],
+    '0': [' '],
 }
 
-var numberToText = function() {
-	
+var repeatString = function(string, num) { return new Array(parseInt(num) + 1).join(string); };
+
+var findValueIndex = function(searchLetter) {
+	var valueIndex;
+
+    for (var arraykey in keyboard) {
+		var lettersArray = keyboard[arraykey]	
+		var index = lettersArray.indexOf(searchLetter);
+        
+        if (index !== -1) { 
+        	valueIndex = {
+        		keyboardNumber: arraykey, 
+        		index: index
+        	}; 
+
+            break;
+        }
+	}
+
+	return valueIndex;
 }
+
+var textToNumber = function (inputText) {
+    var processedText = '';
+    var count, lastLetter;
+
+    var lowerInput = inputText.toLowerCase(); 
+
+    _.forEach(lowerInput, function (value, key) {
+        var valueIndex = findValueIndex(value);
+
+		if(valueIndex) {
+			valueIndex.index += 1; // arrays begins on zero, we have to increase the element before to do the next operation
+			var multipliedString = repeatString(valueIndex.keyboardNumber, valueIndex.index);
+			processedText += multipliedString;
+		}
+    });
+
+    return processedText;
+};
 
 router.post('/number', function (req, res, next) {
-    res.json({});
+    var params = req.body;
+    var result = textToNumber(params.text);
+    return res.json({});
 });
 
 router.post('/text', function (req, res, next) {
-    res.json({});
+    var params = req.body;
+    var result = textToNumber(params.text);
+    return res.json({});
 });
 
-module.exports = router;
+exports.repeatString = repeatString;
+exports.findValueIndex = findValueIndex;
+exports.keyboard = keyboard;
+exports.textToNumber = textToNumber;
+exports.router = router;
