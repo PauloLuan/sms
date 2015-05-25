c9b4f3701d5138ca357f77df988eaaaf0a15e750
Paulo Luan <pauloluan.inova@gmail.com>
2015-05-25 09:20:22 -0300

    fixed module to export all functions as a unique object

--- sms-server/routes/sms.js
+++ sms-server/routes/sms.js
@@ -1,12 +1,13 @@
 'use strict';
 
-var exports = module.exports = {};
+var self = this;
 
 var express = require('express');
-var router = express.Router();
+self.router = express.Router();
+
 var _ = require('lodash');
 
-var keyboard = {
+self.keyboard = {
     '2': ['a', 'b', 'c'],
     '3': ['d', 'e', 'f'],
     '4': ['g', 'h', 'i'],
@@ -18,15 +19,15 @@ var keyboard = {
     '0': [' '],
 }
 
-var repeatString = function (string, num) {
+self.repeatString = function (string, num) {
     return new Array(parseInt(num) + 1).join(string);
 };
 
-var findValueIndex = function (searchLetter) {
+self.findValueIndex = function (searchLetter) {
     var valueIndex;
 
-    for (var arraykey in keyboard) {
-        var lettersArray = keyboard[arraykey]
+    for (var arraykey in self.keyboard) {
+        var lettersArray = self.keyboard[arraykey]
         var index = lettersArray.indexOf(searchLetter);
 
         if (index !== -1) {
@@ -42,18 +43,18 @@ var findValueIndex = function (searchLetter) {
     return valueIndex;
 }
 
-var textToNumber = function (inputText) {
+self.textToNumber = function (inputText) {
     var processedText = '';
     var count, lastLetter;
 
     var lowerInput = inputText.toLowerCase();
 
     _.forEach(lowerInput, function (value, key) {
-        var valueIndex = findValueIndex(value);
+        var valueIndex = self.findValueIndex(value);
 
         if (valueIndex) {
             valueIndex.index += 1; // arrays begins on zero, we have to increase the element before to do the next operation
-            var multipliedString = repeatString(valueIndex.keyboardNumber, valueIndex.index);
+            var multipliedString = self.repeatString(valueIndex.keyboardNumber, valueIndex.index);
 
             // must add an underscore when the sequence is the same as the last number.
             if (processedText.slice(-1) == valueIndex.keyboardNumber) {
@@ -67,7 +68,7 @@ var textToNumber = function (inputText) {
     return processedText;
 };
 
-var isOnlyNumbersOrUnderscore = function (input) {
+self.isOnlyNumbersOrUnderscore = function (input) {
     var isValid;
 
     // TODO: refactor this for, by caching the lenght into variable
@@ -83,7 +84,8 @@ var isOnlyNumbersOrUnderscore = function (input) {
         if (isValid === false) {
             break;
         }
-    };
+    }
+    ;
 
     if (_.isNull(isValid)) {
         isValid = false;
@@ -92,99 +94,95 @@ var isOnlyNumbersOrUnderscore = function (input) {
     return isValid;
 }
 
-var splitNumberSequences = function (inputArray) {
+self.splitNumberSequences = function (inputArray) {
     var result = [],
         lastIndex = 0;
 
-	for (var i = 0, len = inputArray.length; i < len; i++) { 
-    	var value = inputArray[i];
-    	var isNumber = (_.isNumber(parseInt(value))) && !(_.isNaN(parseInt(value)));
-    	var isUnderscore = value === '_';
+    for (var i = 0, len = inputArray.length; i < len; i++) {
+        var value = inputArray[i];
+        var isNumber = (_.isNumber(parseInt(value))) && !(_.isNaN(parseInt(value)));
+        var isUnderscore = value === '_';
 
-    	if(!isNumber && value !== '_') {
-    		result = null;
-    		break;
-    	}
+        if (!isNumber && value !== '_') {
+            result = null;
+            break;
+        }
 
-        if(!result[lastIndex] && result[lastIndex] !== '') {
+        if (!result[lastIndex] && result[lastIndex] !== '') {
             result.push('');
         }
 
-		var isLastTheSame = value === result[lastIndex].slice(-1);
-		var isEmpty = _.isEmpty(result[lastIndex]);
-    	
-    	if (isLastTheSame || isEmpty) {
-            if(!isUnderscore) result[lastIndex] += value;
-        } 
-        else {
-            if(!isUnderscore || !isLastTheSame) { 
-                lastIndex ++;
-                result.push('');
-                if(!isUnderscore) result[lastIndex] += value;
-            }        			
-    	}
-    };
+        var isLastTheSame = value === result[lastIndex].slice(-1);
+        var isEmpty = _.isEmpty(result[lastIndex]);
 
-    if(result) {
-	    result = _.without(result, ''); // clear empty elements from array
+        if (isLastTheSame || isEmpty) {
+            if (!isUnderscore) result[lastIndex] += value;
+        }
+        else {
+            if (!isUnderscore || !isLastTheSame) {
+                lastIndex++;
+                result.push('');
+                if (!isUnderscore) result[lastIndex] += value;
+            }
+        }
+    }
+    ;
+
+    if (result) {
+        result = _.without(result, ''); // clear empty elements from array
     }
 
     return result;
 };
 
-var getLetterByNumberSequence = function(sequence) {
-    if(!allTheSame(sequence)) {
+self.getLetterByNumberSequence = function (sequence) {
+    if (!self.allTheSame(sequence)) {
         return null;
     }
     var value;
-    var number = array[0];
+    var number = sequence[0];
     var len = sequence.length;
-    var letterArray = keyboard[number]; 
-    
-    if(letterArray) {
-        value = letterArray[len];        
+    var letterArray = self.keyboard[number];
+
+    if (letterArray) {
+        value = letterArray[len - 1];
     }
 
     return value;
 };
 
-function allTheSame(array) {
-    var first = array[0];
-    return array.every(function(element) {
-        return element === first;
-    });
+self.allTheSame = function (array) {
+    for(var i = 1, len = array.length; i < len; i++){
+        if (array[i] !== array[0])
+            return false;
+    };
+
+    return true;
 };
 
-var numberToText = function (inputNumber) {
+self.numberToText = function (inputNumber) {
     var result = '';
     var lowerInput = inputNumber.toLowerCase();
-    var splittedArray = splitNumberSequences(lowerInput);
+    var splittedArray = self.splitNumberSequences(lowerInput);
 
     _.forEach(splittedArray, function (value, key) {
-        var sequenceResult = getLetterByNumberSequence(value);
-        if(sequenceResult && sequenceResult != '') result += sequenceResult;
+        var sequenceResult = self.getLetterByNumberSequence(value);
+        if (sequenceResult && sequenceResult != '') result += sequenceResult;
     });
 
-    return processedText;
-}
+    return result;
+};
 
-router.post('/number', function (req, res, next) {
+self.router.post('/number', function (req, res, next) {
     var params = req.body;
-    var result = numberToText(params.text);
+    var result = self.numberToText(params.text);
     return res.json(result);
 });
 
-router.post('/text', function (req, res, next) {
+self.router.post('/text', function (req, res, next) {
     var params = req.body;
-    var result = textToNumber(params.text);
+    var result = self.textToNumber(params.text);
     return res.json(result);
 });
 
-exports.isOnlyNumbersOrUnderscore = isOnlyNumbersOrUnderscore;
-exports.splitNumberSequences = splitNumberSequences;
-exports.repeatString = repeatString;
-exports.findValueIndex = findValueIndex;
-exports.keyboard = keyboard;
-exports.textToNumber = textToNumber;
-exports.numberToText = numberToText;
-exports.router = router;
+module.exports = self;
